abff5427f98b1387b50e460e61ba92f51eba54cf
AlexanderL <al.x.300000@gmail.com>
2022-09-24 14:03:49 -0400

    refactor

--- amazon.py
+++ amazon.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import json
 
 options = Options()
 options.headless = True
@@ -13,6 +14,8 @@ html = driver.page_source
 soup = BeautifulSoup(html,"html.parser")
 
 
+dic = {}
+
 with open("output.html", 'wt', encoding='utf-8') as html_file:
     for line in soup.prettify():
         html_file.write(line)
@@ -21,27 +24,26 @@ links = []
 for a in soup.find_all('a', {"class": "a-link-normal s-no-outline"}):
     links.append(a["href"])
 
+for i in range(len(links)-30):
+    driver.get(f"https://www.amazon.ca{links[i]}")
+    html = driver.page_source
 
-driver.get(f"https://www.amazon.ca{links[0]}")
-html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
 
-soup = BeautifulSoup(html, "html.parser")
+    title = soup.find("span", {"id": "productTitle"}).get_text()
+    tables = soup.find("table", {"class":"a-normal a-spacing-micro"}).find_all("span")
 
-tables = soup.find("table", {"class":"a-normal a-spacing-micro"}).find_all("span")
-# children = tables.findChildren("span")
-
-for count, item in enumerate(tables):
-    
-    if count % 2 == 0:
-        print("\n")
-        print(item.get_text(),end = ": ")
-    else:
-        print(item.get_text(),end = "")
-
-   
+    temp = {}
+    for i in range(0,len(tables),2):
+        temp[tables[i].get_text()]=tables[i+1].get_text()
+    dic[title]=temp
 
 
 
+
+dic =json.dumps(dic)
+
+print(dic)
 
 
 # NEXT STEP: Find a-size-base a-text-bold from within this object
@@ -49,5 +51,4 @@ for count, item in enumerate(tables):
 
 driver.quit()
 
-# for item in links:
 
